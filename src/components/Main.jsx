93a7297f18990b40db890f1d603c83bc30383793
Justin Pawlowski <justinpawlowski23@gmail.com>
2025-08-01 16:30:24 -0500

    designing the layout of the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,32 @@
 
 
+
+
 export default function Main() {
+
+
+
     return (
-        // 3 sections of data
-        <h1>main component</h1>
+        <main>
+            <div className='main-container'>
+                <div className='left'>
+                    <section className='top-left'>
+                        <h3>This section could have a chart(s) that include long-term goals, daily goals, etc.</h3>
+                    </section>
+                    
+                    <section className='bottom-left'>
+                        <h3>This could show the planned meals for the day or an area to enter in the food that has been eaten</h3>
+                    </section>
+                </div>
+
+                <section className='right'>
+                    <h3>This section could show workouts recently done, scheduled workouts, or other similar things</h3>
+                </section>
+            </div>
+
+                <section className='consistency'>
+                    <h3>This could show the consistency on a calendar</h3>
+                </section>
+        </main>
     )
 }
