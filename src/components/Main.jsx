832f8b40266a0fd84bac3bb0f98ebf9442d0ffa3
Justin Pawlowski <justinpawlowski23@gmail.com>
2025-08-02 13:01:14 -0500

    figuring out adding charts for certain areas of the site

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,62 @@
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-
+// Register required components
+ChartJS.register(
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
 
 
 export default function Main() {
 
+      const data = {
+    labels: ['Jun', 'Jul', 'Aug'],
+    datasets: [
+      {
+        id: 1,
+        label: 'Dataset 1',
+        data: [5, 6, 7],
+        borderColor: 'rgba(75,192,192,1)',
+        backgroundColor: 'rgba(75,192,192,0.2)',
+      },
+      {
+        id: 2,
+        label: 'Dataset 2',
+        data: [3, 2, 1],
+        borderColor: 'rgba(255,99,132,1)',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+      },
+    ],
+  };
 
-    
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+
+
+
+
     return (
+
         <main>
             <div className='main-container'>
                 <div className='left'>
@@ -26,6 +76,7 @@ export default function Main() {
 
                 <section className='consistency'>
                     <h3>This could show the consistency on a calendar</h3>
+                    <Line datasetIdKey="id" data={data} options={options} />
                 </section>
         </main>
     )
