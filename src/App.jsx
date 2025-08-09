1ba187e2220f1d5d3a561fbbb3b7b798b8b8f10b
Justin Pawlowski <justinpawlowski23@gmail.com>
2025-08-09 12:57:26 -0500

    adding react router to make multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
-import Header from './components/header/Header'
-import Main from './components/main-section/Main'
-import Footer from './components/footer/Footer'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+import Header from '/src/pages/components/header/Header'
+import Footer from '/src/pages/components/footer/Footer'
+import Dashboard from '/src/pages/dashboard/Dashboard'
+import NotFoundPage from '/src/pages/not-found/NotFoundPage'
+import NutritionPage from './pages/nutrition/NutritionPage'
+import FitnessPage from './pages/fitness/FitnessPage'
+import UserPage from './pages/user/UserPage'
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { 
+        index: true, // sets as default page
+        element: <Dashboard /> 
+      },
+
+      { 
+        path: '/nutrition',
+        element: <NutritionPage /> 
+      },
+
+      { 
+        path: '/fitness',
+         element: <FitnessPage />
+      },
+
+      {
+        path: '/user',
+        element: <UserPage />
+      }
+    ]
+  }
+]);
 
 export default function App() {
 
   return( 
     <>
-      <Header />
-      <Main />
-      <Footer />
+      {/* <Header /> */}
+      <RouterProvider router={router}/>
+      {/* <Footer />  */}
     </>
 
   )
