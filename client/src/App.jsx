1cec7a0cb6deca8c5370dc713ee6841592d7ab91
Justin Pawlowski <justinpawlowski23@gmail.com>
2025-08-11 22:14:57 -0500

    adding a login and signup page for future authorization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
+// styles
+import './styles/index.css'
+
 // pages and components
 import Header from '/src/pages/components/header/Header'
 import Footer from '/src/pages/components/footer/Footer'
@@ -8,17 +11,30 @@ import NotFoundPage from '/src/pages/not-found/NotFoundPage'
 import NutritionPage from './pages/nutrition/NutritionPage'
 import FitnessPage from './pages/fitness/FitnessPage'
 import UserPage from './pages/user/UserPage'
-import ProgressHistory from './pages/ProgressHistoryPage/ProgressHistory'
+import ProgressHistory from './pages/progress-history/ProgressHistory'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/SIgnup'
+
 
 function Layout() {
   return (
-    <>
+    // to set up the header and footer components on all the pages 
+    <div className='main-body'>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
+  );
+}
+
+function AuthLayout() {
+  // No header or footer 
+  return (
+    <main className="auth-layout-main">
+      <Outlet />
+    </main>
   );
 }
 
@@ -28,36 +44,25 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <NotFoundPage />,
     children: [
-      { 
-        index: true, // sets as default page
-        element: <Dashboard /> 
-      },
-
-      { 
-        path: '/nutrition',
-        element: <NutritionPage /> 
-      },
-
-      { 
-        path: '/fitness',
-         element: <FitnessPage />
-      },
-
-      {
-        path: '/user',
-        element: <UserPage />
-      },
-
-      {
-        path: '/progress-history',
-        element: <ProgressHistory />
-      }
-
+      { index: true, element: <Dashboard /> },
+      { path: '/nutrition',element: <NutritionPage /> },
+      { path: '/fitness', element: <FitnessPage /> },
+      { path: '/user', element: <UserPage /> },
+      { path: '/progress-history', element: <ProgressHistory /> }
     ]
-  }
+  }, 
+  {
+    path: '/auth',
+    element: <AuthLayout />,   // login/signup without header/footer
+    children: [
+      { path: 'login', element: <Login /> },
+      { path: 'signup', element: <Signup /> },
+    ],
+  },
 ]);
 
 export default function App() {
+
 
   return( 
     <>
