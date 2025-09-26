880a21e5d76b78ae6b7d28497f3745d09ac14159
Justin Pawlowski <justinpawlowski23@gmail.com>
2025-09-26 15:28:55 -0500

    adding to the fitness page

--- client/src/pages/fitness/FitnessPage.jsx
+++ client/src/pages/fitness/FitnessPage.jsx
@@ -1,16 +1,24 @@
 import './fitness-page.css'
 
 export default function FitnessPage() {
+    function subChoice() {
+        console.log('choice selected')
+    }
+
     return (
         <div className='fitness-main'>
-            <form className='fitness-select-div'>
+            <h1>Fitness</h1>
+
+            <form onSubmit={subChoice} className='fitness-select-div'>
                 <label>Select a workout</label>
                 <select>
                     <option>Running</option>
                     <option>Workout</option>
                     <option>Other</option>
                 </select>
+                <button>Select</button>
             </form>
+
         </div>
     )
 }
