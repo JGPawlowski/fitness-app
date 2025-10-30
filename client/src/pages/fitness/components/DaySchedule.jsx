be45f645837ac3d9ca6e26f585da7c9b636e872e
Justin Pawlowski <justinpawlowski23@gmail.com>
2025-10-30 15:28:45 -0500

    day schedule

---
+++ client/src/pages/fitness/components/DaySchedule.jsx
@@ -0,0 +1,12 @@
+
+
+export default function DaySchedule(props) {
+
+
+    return (
+        <div>
+            <h1>{props.day}</h1>
+
+        </div>
+    )
+}
--- client/src/pages/fitness/components/PlanFitness.jsx
+++ client/src/pages/fitness/components/PlanFitness.jsx
@@ -1,43 +1,47 @@
 import { useState } from "react"
 
+import DaySchedule from "./DaySchedule"
+
 
 export default function PlanFitness() {
 
-    // show the schedule / rendure the schedule component when it is made
-    const [showDayData, setShowDayData] = useState(false)
-    const [day, setday] = useState('Current Day')
+    const today = new Date()
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    // state for day
+    const [day, setday] = useState(days[today.getDay()]) 
+    
 
+    
     function dayData(day) {
         // use for showing the data for the correct day that is clicked
         // could set up the ability to set repeatable activities
         setday(day)
         // need to change how this works
-        setShowDayData(prev => !prev)
     }
 
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    let dayBtns = days.map(day => (
-        <button key={day} onClick={() => dayData(`${day}`)}>{day}</button>
-    ))
+    function getBtns() {
+        let dayBtns = days.map(dayName => (
+            <button 
+                key={dayName}
+                style={dayName === day ? {backgroundColor: '#f15152', color:'white'} : null}
+                onClick={() => dayData(`${dayName}`)}
+             >{dayName}</button>
+        ))
+
+        return dayBtns
+    }
+    
 
 
     return (
         <div className='plan-fitness-main'>
             <div className='day-of-week-planning'>
-                {/* <button onClick={() => dayData('monday')}>Monday</button>
-                <button onClick={() => dayData('tuesday')}>Tuesday</button>
-                <button onClick={() => dayData('wednesday')}>Wednesday</button>
-                <button onClick={() => dayData('thursday')}>Thursday</button>
-                <button onClick={() => dayData('friday')}>Friday</button>
-                <button onClick={() => dayData('saturday')}>Saturday</button>
-                <button onClick={() => dayData('sunday')}>Sunday</button>  */}
-                { dayBtns }
+                { getBtns() }
             </div> 
-            <p>After the day of the week is clicked, show a time schedule of what the plan is for the day</p>
-            <p>Automatically start with the current day to be the button selected</p>
-            <h1>{day}</h1>
 
-            { showDayData ? <h1>Showing...</h1> : <h1>Not Showing...</h1>}
+            <DaySchedule 
+                day={day}
+            /> 
         </div>
     )
 }
